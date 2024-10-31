f1aa3314bc40593220bdcffeb266be920c89fa03
Van Hoang Anh <84760955+vanhoanganhz@users.noreply.github.com>
2024-10-31 11:47:09 +0700

    Create bt2.java

---
+++ bt2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class kiemtrachuoi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("nhap ki tu de kiem tra chuoi: ");
+        if (scanner.hasNext()) {
+            String input = scanner.next();
+            System.out.println("ki tu vua nhap la chuoi: " + input);
+        } else {
+            System.out.println("ki tu nhap khong phai la chuoi");
+        }
+
+        scanner.close();
+    }
+}
