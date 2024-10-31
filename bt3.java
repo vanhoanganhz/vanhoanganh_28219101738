e3c296f665b05540832fc95db7fb68017a3d7d48
Van Hoang Anh <84760955+vanhoanganhz@users.noreply.github.com>
2024-10-31 11:49:10 +0700

    Create bt3.java

---
+++ bt3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class kiemtrasonguyen {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("nhap ki tu de kiem tra so nguyen: ");
+        if (scanner.hasNextInt()) {
+            int so = scanner.nextInt();
+            System.out.println("ky tu vua nhap la so nguyen: " + so);
+        } else {
+            System.out.println("ky tu vua nhap khong phai la so nguyen");
+        }
+
+        scanner.close();
+    }
+}
